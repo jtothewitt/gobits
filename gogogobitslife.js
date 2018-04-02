3bc6c90b75c9ac719bb6716fdd3cced66e0924f1
John Witt <jwitt10@gmail.com>
2018-04-02 19:00:10 +0100

    Create gogogobitslife.js

---
+++ gogogobitslife.js
@@ -0,0 +1,36 @@
+/* gogogobitslife.js */
+
+function executeAt(time, func) {
+  var currentTime = new Date().getTime();
+  if (currentTime > time) {
+    console.error("Time is in the Past");
+    return false;
+  }
+  setTimeout(func, time - currentTime);
+  return true;
+}
+
+$(document).ready(function() {
+  executeAt(new Date().setTime(new Date().getTime() + 1000), function() {
+    alert(none);
+  });
+});
+
+var words = [
+    "be a gobit!",
+    "run a 10k.",
+    "call mom more.",
+    "get a promotion.",
+    "buy a house."
+  ],
+  i = 0;
+
+setInterval(function() {
+  // \/ \/ callback function
+  $("#goalHop").fadeOut(600, function() {
+    // if i = last index ? i = 0 else i++
+    $(this)
+      .text(words[i === words.length - 1 ? (i = 0) : (i += 1)])
+      .fadeIn(600);
+  });
+}, 2700);
